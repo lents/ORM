46ac554ceb70ca95f34b8a773245baf7274a6348
Elena Sycheva <Elena_Tsygankova@hotmail.com>
2024-04-05 06:46:18 +0300

    Добавьте файлы проекта.

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class UserData : IUserData
+    {
+        private readonly ISqlDataAccess _db;
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<UserModel>> GetUsers() =>
+            _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+        public async Task<UserModel?> GetUser(int id)
+        {
+            var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new { Id = id });
+            return results.FirstOrDefault();
+        }
+
+        public async Task InsertUser(UserModel user) =>
+            await _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+        public async Task UpdateUser(UserModel user) =>
+            await _db.SaveData("dbo.spUser_Update", user);
+
+        public async Task DeleteUser(int id) =>
+            await _db.SaveData("dbo.spUser_Delete", new { Id = id });
+
+    }
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,7 @@
+﻿if not exists (SELECT 1 FROM [dbo].[User])
+begin
+	insert into [dbo].[User](FirstName, LastName)
+	values ('Tim', 'Black'),
+	('Sandy', 'Marble'),
+	('Tom', 'Smith')
+end
